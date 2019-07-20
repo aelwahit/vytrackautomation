ef5ec84c976dbd33c2fd0d8d6329e9993d20b5c8
Adem Kahraman <aelwahit@gmail.com>
2019-07-19 20:09:17 -0500

    July15

--- src/test/java/com/vytrack/tests/smoke_tests/MenuOptionsTest.java
+++ src/test/java/com/vytrack/tests/smoke_tests/MenuOptionsTest.java
@@ -8,21 +8,19 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.testng.annotations.*;
 
 import java.util.concurrent.TimeUnit;
 
 public class MenuOptionsTest {
 
 
+    WebDriver driver;
 
-        WebDriver driver;
 
-
-    @BeforeClass
-    public void setup(){
+    @BeforeMethod
+    public void setup() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
@@ -36,58 +34,138 @@ public class MenuOptionsTest {
 //        wait.until(ExpectedConditions.visibilityOf(element));
 //    }
 
+
     @Test
-    public void Test1(){
+   public void MenuOptionsDriver() {
         driver.get("http://qa2.vytrack.com/user/login");
         driver.findElement(By.cssSelector("[name='_username']")).sendKeys("user165");
         driver.findElement(By.cssSelector("[id='prependedInput2']")).sendKeys("UserUser123");
         driver.findElement(By.cssSelector("[name='_submit']")).click();
-        Library.sleep(5);
-       //driver.findElement(By.xpath("//*[text()='Vehicles']")).click();
+        Library.sleep(2);
+        //driver.findElement(By.xpath("//*[text()='Vehicles']")).click();
         driver.findElement(By.cssSelector("span[class='title title-level-1']")).click();
-       driver.findElement(By.cssSelector("span[class='title title-level-2']")).click();
-        Library.sleep(4);
-       String expected="Car - Entities - System - Car - Entities - System";
-       String actual=driver.getTitle();
-       Library.verifyEquals(expected,actual);
+        driver.findElement(By.cssSelector("span[class='title title-level-2']")).click();
+        Library.sleep(2);
+        String expected = "Car - Entities - System - Car - Entities - System";
+        String actual = driver.getTitle();
+        //  Library.verifyEquals(expected,actual);
+        Assert.assertEquals(expected, actual);
 
-       Library.sleep(4);
-       driver.findElement(By.xpath("//*[@id=\"main-menu\"]/ul/li[2]/a/span")).click();
-       driver.findElement(By.xpath("//*[text()='Accounts']")).click();
 
-       Library.sleep(4);
-        System.out.println("=============");
-        System.out.println( driver.getTitle());
-        System.out.println(  driver.getTitle().contains("Accounts")? "PASSED":"FAILED"   );
-        System.out.println("=============");
+        Library.sleep(2);
+        driver.findElement(By.xpath("//*[@id=\"main-menu\"]/ul/li[1]/a/span")).click();
+        Library.sleep(2);
+        driver.findElement(By.xpath("//*[text()='Accounts']")).click();
+        Library.sleep(2);
+        String actualAccounts = driver.findElement(By.xpath("//h1[@class='oro-subtitle']")).getText();
+        String expectedAccounts = "Accounts";
+        Assert.assertEquals(expectedAccounts, actualAccounts);
+        Library.sleep(2);
 
 
         driver.findElement(By.xpath("//*[@id=\"main-menu\"]/ul/li[2]/a/span")).click();
         driver.findElement(By.xpath("//*[text()='Contacts']")).click();
-        Library.sleep(4);
-        System.out.println("=============");
-        System.out.println( driver.getTitle());
-        System.out.println(  driver.getTitle().contains("Contacts")? "PASSED":"FAILED"   );
-        System.out.println("=============");
+        Library.sleep(2);
+        String actualContacts = driver.findElement(By.xpath("//h1[text()='Contacts']")).getText();
+        String expectedContacts = "Contacts";
+        Assert.assertEquals(expectedContacts, actualContacts);
+        Library.sleep(2);
 
-        Library.sleep(4);
+
+        Library.sleep(2);
         driver.findElement(By.xpath("//*[@id=\"main-menu\"]/ul/li[3]/a/span")).click();
         driver.findElement(By.xpath("//*[text()='Calendar Events']")).click();
+        Library.sleep(1);
+        String actualCalendarEvents = driver.findElement(By.xpath("//h1[text()='Calendar Events']")).getText();
+        String expectedCalendarEvents = "Calendar Events";
+        Assert.assertEquals(expectedCalendarEvents, actualCalendarEvents);
+        Library.sleep(1);
+
+    }
+
+@Test
+    public void MenuOptionsStoreManager(){
+
+        driver.get("http://qa2.vytrack.com/user/login");
+        driver.findElement(By.cssSelector("[name='_username']")).sendKeys("storemanager99");
+        driver.findElement(By.cssSelector("[id='prependedInput2']")).sendKeys("UserUser123");
+        driver.findElement(By.cssSelector("[name='_submit']")).click();
+        Library.sleep(5);
+
+        driver.findElement(By.xpath("//span[@class='title title-level-1' and contains(text(),'Dashboards')]")).click();
+        String actualDashboard = driver.findElement(By.cssSelector("h1[class='oro-subtitle']")).getText();
+        String expectedDashboard = "Dashboard";
+         Assert.assertEquals(expectedDashboard,actualDashboard);
+         Library.sleep(2);
+
+         driver.findElement(By.xpath("//span[@class='title title-level-1' and contains(text(),'Fleet')]")).click();
         Library.sleep(4);
-        System.out.println("=============");
-        System.out.println( driver.getTitle());
-        System.out.println(  driver.getTitle().contains("Calendar Events")? "PASSED":"FAILED"   );
-        System.out.println("=============");
+        driver.findElement(By.xpath("//span[text()='Vehicles']")).click();
+       Library.sleep(4);
+        Assert.assertEquals("Car - Entities - System - Car - Entities - System", driver.getTitle());
+
+    Library.sleep(4);
+    driver.findElement(By.xpath("//span[@class='title title-level-1' and contains(text(),'Customers')]")).click();
+    Library.sleep(3);
+    driver.findElement(By.xpath("//*[text()='Accounts']")).click();
+    Library.sleep(2);
+    String actualAccounts = driver.findElement(By.xpath("//h1[@class='oro-subtitle']")).getText();
+    String expectedAccounts = "Accounts";
+    Assert.assertEquals(expectedAccounts, actualAccounts);
+    Library.sleep(2);
+
+
+
+
+
+
+
 
 
 
 
 
     }
-@AfterClass
-    public void close(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@AfterMethod
+    public void close() {
     Library.sleep(2);
-        driver.close();
+    driver.close();
 }
 
 
